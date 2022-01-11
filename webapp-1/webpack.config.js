d5cc018f782b9b3cb516d00ec19b34c2b5a54bfd
Vio <vio@beanon.com>
2022-01-11 23:27:34 +0100

    fix: Update stats output

--- webapp-1/webpack.config.js
+++ webapp-1/webpack.config.js
@@ -102,6 +102,12 @@ module.exports = (_, { mode }) => {
     devServer: {
       hot: true,
       inline: true
+    },
+    stats: {
+      assets: true,
+      entrypoints: true,
+      chunks: true,
+      modules: true
     }
   };
 };
